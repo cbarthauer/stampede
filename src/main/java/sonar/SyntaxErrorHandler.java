ffc3aaec1e5173823b650c46eb82af040b5e0d1a
Chris Barthauer <cbarthauer@server>
2013-05-23 07:30:05 -0400

    Updated license headers.

--- src/main/java/sonar/SyntaxErrorHandler.java
+++ src/main/java/sonar/SyntaxErrorHandler.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright (C) 2013 Chris Barthauer <mumpsstampede@gmail.com>
+ *
+ * This file is part of STAMPEDE.
+ *
+ * STAMPEDE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * STAMPEDE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with STAMPEDE.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *     Chris Barthauer - Initial API and implementation.
+ */
 package sonar;
 
 import analyzer.MumpsSyntaxError;
@@ -13,7 +34,7 @@ import org.sonar.api.resources.Project;
  */
 public interface SyntaxErrorHandler {
     /**
-     * Save the give syntaxErrors in Sonar.
+     * Save the given syntaxErrors in Sonar.
      * 
      * @param project associated with syntaxErrors
      * @param context in which to save results
